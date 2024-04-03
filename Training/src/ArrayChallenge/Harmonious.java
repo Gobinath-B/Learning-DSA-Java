26e68f47de9268a9497b69e47d64ef3fa7e41f56
Gobinath B <princegopinath42@gmail.com>
2024-04-03 22:19:07 +0530

    feat sliding window

---
+++ Training/src/ArrayChallenge/Harmonious.java
@@ -0,0 +1,26 @@
+package ArrayChallenge;
+
+import java.util.Arrays;
+
+public class Harmonious {
+
+        public int findLHS(int[] nums) {
+            int left = 0, answer = 0;
+            Arrays.sort(nums);
+
+            for (int right = 0; right < nums.length; right++) {
+
+                if (nums[right] - nums[left] == 1) {
+                    answer = Math.max(answer, right - left + 1);
+                }
+
+                while (nums[right] - nums[left] > 1) {
+                    left++;
+                }
+
+            }
+
+            return answer;
+
+        }
+    }
