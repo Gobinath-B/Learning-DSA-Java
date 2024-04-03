dcdbc07832550451344f070a7a1402b8927c39cb
Gobinath B <princegopinath42@gmail.com>
2024-04-03 22:21:02 +0530

    feat sliding window

--- Training/src/ArrayChallenge/Harmonious.java
+++ Training/src/ArrayChallenge/Harmonious.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 public class Harmonious {
 
-        public int findLHS(int[] nums) {
+    public static void main(String[] args) {
+        int arr[] = {1,3,2,2,5,2,3,7};
+        System.out.println(findLHS(arr));
+    }
+        public static int findLHS(int[] nums) {
             int left = 0, answer = 0;
             Arrays.sort(nums);
 
